5d426f256b8f8db38fe28ed48310179f883ab56d
Vinaybannur <vinaybannur143@gmail.com>
2021-11-11 14:01:14 +0530

    changes

--- src/MoviesLists.js
+++ src/MoviesLists.js
@@ -8,22 +8,24 @@ export function MoviesLists({ movies1 ,setmovie }) {
   return (
     <div className="Allmovies">
       {movies1.map(({ name, rating, summary, poster },index) => 
+
       <Movie movieName={name} 
       poster={poster} 
       rating={rating} 
       summary={summary} 
       id={index} 
       deleteButton={ <IconButton onClick={()=>{ 
-        console.log("deleting...",index)
-        const deleteIdx = index;
-        const remainingMovies = movies1.filter((mv,idx)=>idx != deleteIdx)
-        setmovie(remainingMovies);
+        // console.log("deleting...",index)
+        // const deleteIdx = index;
+        // const remainingMovies = movies1.filter((mv,idx)=>idx != deleteIdx)
+        // setmovie(remainingMovies);
       }}
         className="movie-show-button"
         color="error"
         aria-label="more-info">
              < DeleteIcon/>
    </IconButton>}
+
       editButton={<IconButton onClick={()=>{ }}
       className="movie-show-button"
       color="primary"
